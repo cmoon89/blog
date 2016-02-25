34a8de103b0d139d26af8d14af5856ac94e7d9f2
Christian Moon <xc.moon89@gmail.com>
2016-02-25 17:57:35 -0500

    Replace password column to use password digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225224532) do
+ActiveRecord::Schema.define(version: 20160225225540) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "username"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20160225224532) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password"
+    t.string   "password_digest"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
