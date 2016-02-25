52cf861b2a35851e90485625c92f2531f14af035
Christian Moon <xc.moon89@gmail.com>
2016-02-25 17:37:10 -0500

    Make sure email is unique in database

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,6 @@
 class Admin < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	
   validates :username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
